607e3fdb87cfb7edd7e6290576b8475889d9ddbd
Deepanshi1523 <104934054+Deepanshi1523@users.noreply.github.com>
2024-01-29 19:35:35 +0530

    made the basic structure without using components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 
 
 function App() {
-  return <div>Placehorder</div>
+  return <center>
+    <h1>Todo App</h1>
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-6">
+          <input type="text" placeholder="Enter todo here"/>
+        </div>
+        <div class="col-4">
+          <input type="date"/>
+        </div>
+        <div class="col-2">
+          <button type="button" class="btn btn-success">Add</button>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-6">Buy Milk</div>
+        <div class="col-4">4/10/2023</div>
+        <div class="col-2">
+          <button type="button" class="btn btn-danger">Delete</button>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-6">Go To College</div>
+        <div class="col-4">4/10/2023</div>
+        <div class="col-2">
+          <button type="button" class="btn btn-danger">Delete</button>
+        </div>
+      </div>
+    </div>
+  </center>
 }
 
 export default App
