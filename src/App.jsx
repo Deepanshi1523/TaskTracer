4ae37768e26a178cc2299caebfa2eecbd804544a
Deepanshi1523 <104934054+Deepanshi1523@users.noreply.github.com>
2024-01-29 23:23:33 +0530

    made the basic structure using components

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,15 @@
+import AddTodo from "./components/AddTodo"
+import AppName from "./components/AppName"
+import TodoItem1 from "./components/TodoItem1"
+import TodoItem2 from "./components/TodoItem2"
 
 
 function App() {
   return <center>
-    <h1>Todo App</h1>
-    <div class="container text-center">
-      <div class="row">
-        <div class="col-6">
-          <input type="text" placeholder="Enter todo here"/>
-        </div>
-        <div class="col-4">
-          <input type="date"/>
-        </div>
-        <div class="col-2">
-          <button type="button" class="btn btn-success">Add</button>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-6">Buy Milk</div>
-        <div class="col-4">4/10/2023</div>
-        <div class="col-2">
-          <button type="button" class="btn btn-danger">Delete</button>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-6">Go To College</div>
-        <div class="col-4">4/10/2023</div>
-        <div class="col-2">
-          <button type="button" class="btn btn-danger">Delete</button>
-        </div>
-      </div>
-    </div>
+    <AppName/>
+    <AddTodo/>
+    <TodoItem1/>
+    <TodoItem2/>
   </center>
 }
 
