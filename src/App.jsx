42761e6f32265afa4db558b8167df485f9791fe7
Deepanshi1523 <104934054+Deepanshi1523@users.noreply.github.com>
2024-01-31 01:04:16 +0530

    used props

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import AddTodo from "./components/AddTodo";
 import AppName from "./components/AppName";
-import TodoItem1 from "./components/TodoItem1";
-import TodoItem2 from "./components/TodoItem2";
 import "./App.css";
+import TodoItems from "./components/TodoItems";
 
 
 function App() {
+  const todoItems=[
+    {
+      name:"Buy Milk",
+      dueDate:"4/10/2023"
+    },
+    {
+      name:"Go To College",
+      dueDate:"4/10/2023"
+    },
+  ];
   return <center className="todo-container">
     <AppName/>
     <AddTodo/>
-    <div className="items-container">
-      <TodoItem1/>
-      <TodoItem2/>
-    </div>
+    <TodoItems todoItems={todoItems}/>
   </center>
 }
 
