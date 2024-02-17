cdd49406a0775c1b53f787f9e669a175e852e58b
Deepanshi1523 <104934054+Deepanshi1523@users.noreply.github.com>
2024-02-17 07:17:40 +0530

    removed unwanted error

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -5,7 +5,7 @@ function TodoItem({todoName, todoDate, onDeleteClick}){
         <div className="col-6">{todoName}</div>
         <div className="col-4">{todoDate}</div>
         <div className="col-2">
-          <button type="button" class="btn btn-danger kg-button" onClick={()=>onDeleteClick(todoName)}>Delete</button>
+          <button type="button" className="btn btn-danger kg-button" onClick={()=>onDeleteClick(todoName)}>Delete</button>
         </div>
       </div>
     </div>
